65321510c5b101b7cc6e8d975a5792c5efe0f171
LightFex77 <max706genesoviedo@gmail.com>
2023-08-28 10:27:43 -0400

    Agregue el componente de tipo Button

--- frontend/src/components/elements/Button.jsx
+++ frontend/src/components/elements/Button.jsx
@@ -1,7 +1,14 @@
-export const Button = () => {
-    return(
-        <div>
-            <button></button>
-        </div>
-    )
+/* eslint-disable react/prop-types */
+export const Button = ({buttonText, onClick, error, labelText}) => {
+  return (
+    <div className="button-element">
+      <label htmlFor="buttonEelement">
+        {labelText}
+        <button id="buttonElement" onClick={onClick}>{buttonText}</button>
+        <span style={{color:"red", height: "1rem"}}>
+        {error}
+      </span>
+      </label>
+    </div>
+  );
 };
