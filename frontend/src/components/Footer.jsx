f0666ea16179f71ef57beb027ba41ff4ef6d0633
LightFex77 <max706genesoviedo@gmail.com>
2023-08-29 17:09:46 -0400

    Creamos la hoja de estilos para el Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,12 @@
+// import Input from "../components/elements/Input";
+// import Button from "../components/elements/Button";
+
+import "../styles/footer.css"
+
 const Footer = () => {
     return(
         <footer className="footer-container">
-
+            
         </footer>
     )
 }
