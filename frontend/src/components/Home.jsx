eccffe8216fd6913f060ca3fc51e7adc18e7a024
LightFex77 <max706genesoviedo@gmail.com>
2023-08-29 16:24:11 -0400

    Creamos el componente Footer

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,21 @@
-const Home = () => {
-    return(
-        <div>
-            
-        </div>
-    )
-}
+import "../styles/home.css";
+import Footer from "./Footer";
+import SliderSection from "./elements/SliderSection";
 
-export default Home
+const Home = () => {
+  return (
+    <div className="home-container">
+      <SliderSection
+        titleText="Consultar"
+        textContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sit
+              expedita asperiores obcaecati eum quasi totam explicabo voluptatem
+              excepturi architecto, voluptates minus reprehenderit suscipit
+              error, doloribus corporis odit eius ducimus!"
+              buttonText="Consultar"
+      />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
