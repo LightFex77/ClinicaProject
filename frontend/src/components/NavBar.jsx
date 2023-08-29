49e7dded3fd53952344d89a361b4f42be08b066c
LightFex77 <max706genesoviedo@gmail.com>
2023-08-29 11:56:23 -0400

    Agregue un logo

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom"
 import "../styles/nav.css";
+import logo from "../assets/pngwing.png"
 
 const NavBar = () => {
     return(
         <div className="nav-container">
             <ul>
+                <li><img src={logo}/>Miracles</li>
                 <li><NavLink className={({isActive}) => isActive ? "active-link" : ""} to="/">Inicio</NavLink></li>
                 <li><NavLink className={({isActive}) => isActive ? "active-link" : ""} to="/specialists">Especialistas</NavLink></li>
                 <li><NavLink className={({isActive}) => isActive ? "active-link" : ""} to="/services">Servcios</NavLink></li>
