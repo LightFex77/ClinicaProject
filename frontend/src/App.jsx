dd4b9d7954375160bab05f896fae878d40dc38a2
LightFex77 <max706genesoviedo@gmail.com>
2023-09-01 18:02:42 -0400

    Cree un estilo css para el login y register, y quite sus rutas de app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import Register from "./components/Register";
-import Login from "./components/Login";
 import Home from "./components/home/Main"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/home/Header";
@@ -13,12 +11,6 @@ const App = () => {
     <BrowserRouter>
     <Header />
     <Routes>
-      <Route path="/registrarse" element={<div className="clinic-container">
-      <Register/>
-    </div>}/>
-      <Route path="/iniciar-sesion" element={<div className="clinic-container">
-      <Login/>
-    </div>}/>
     <Route path= "/" element = {<Home />} />
     <Route path="/consultas" element = {<Consults />}/>
     </Routes>
