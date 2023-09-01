a9701470ad82f98fe90b9e13d27b225946f4152d
LightFex77 <max706genesoviedo@gmail.com>
2023-09-01 18:32:55 -0400

    Agregue los boton des login en el header

--- frontend/src/components/home/Header.jsx
+++ frontend/src/components/home/Header.jsx
@@ -1,21 +1,24 @@
 import "../../styles/headerNav.css";
 import image from "../../assets/images/pngwing.png";
 import NavBar from "./NavBar";
-
+import Button from "../elements/Button";
 
 const Header = () => {
   return (
     <div className="top-page">
-    <header className="header-container">
-      <div className="header-logo">
-      <img src={image}/>
-        Miracles
-       
-      </div>
-    </header>
-    <NavBar />
+      <header className="header-container">
+        <div className="header-logo">
+          <img src={image} />
+          Miracles
+        </div>
+        <div className="header-btn">
+          <Button buttonText="Iniciar Sesion" />
+          <Button buttonText="Registrarse"></Button>
+        </div>
+      </header>
+      <NavBar />
     </div>
-    )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/components/home/NavBar.jsx
+++ frontend/src/components/home/NavBar.jsx
@@ -1,4 +1,5 @@
-import {NavLink} from "react-router-dom"
+import {NavLink} from "react-router-dom";
+// import Button from "../elements/Button";
 
 const NavBar = () => {
   const classChange = (classObject) => {
@@ -9,7 +10,7 @@ const NavBar = () => {
       <NavLink className={({isActive}) => classChange(isActive)} to="/">Inicio</NavLink>
       <NavLink className={({isActive}) => classChange(isActive)} to="/consultas">Consultas</NavLink>
       <NavLink className={({isActive}) => classChange(isActive)} to="/acerca-de">Acerca de</NavLink>
-      <NavLink className={({isActive}) => classChange(isActive)} to="/iniciar-sesion">Iniciar Sesion</NavLink>
+      {/* <Button buttonText="Iniciar Sesion"/> */}
     </nav>
   )
 }
