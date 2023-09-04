bd15e6ba362e1cee5d4a7e013d91831883a821ee
LightFex77 <max706genesoviedo@gmail.com>
2023-09-04 13:16:54 -0400

    Ajuste algunas cosas del filter

--- frontend/src/components/consults/ConsultsFilter.jsx
+++ frontend/src/components/consults/ConsultsFilter.jsx
@@ -2,31 +2,47 @@ import Button from "../elements/Button";
 
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
-const ConsultsFilter = ({ setSelectOption, toLookFor, handleFilterChange, dentists }) => {
+const ConsultsFilter = ({
+  setSelectOption,
+  toLookFor,
+  handleFilterChange,
+  dentists,
+}) => {
+  const uniqueValues = [...new Set(dentists.map(item => item[toLookFor]))];
+
   return (
     <div className="consults-filter">
-    <select onChange={handleFilterChange}>
-      <option value="todos">Todos</option> {/* Opción "Todos" */}
-      <option value="specialty">Servicios</option>
-      <option value="location">Ubicación</option>
-    </select>
-    <select
-      onChange={(e) => setSelectOption(e.target.value)}
-      disabled={toLookFor === "todos"} // Deshabilitar el segundo select si la opción seleccionada es "Todos"
-    >
-      <option value="" hidden>
-        {toLookFor === "specialty" ? "Selecciona una especialidad" : "Selecciona una ubicación"}
+      <div className="consults-my-quotes">
+        <p>Paciente: Juanito Quinto</p>
+        <Button buttonText="Ver mis consultas" />
+      </div>
+      <div className="serach-filter">
+        <p>Buscar por:</p>
+
+        <span>
+          <select onChange={handleFilterChange}>
+            <option value="todos">Todos</option> {/* Opción "Todos" */}
+            <option value="specialty">Servicios</option>
+            <option value="location">Ubicación</option>
+          </select>
+          <select
+    onChange={(e) => setSelectOption(e.target.value)}
+    disabled={toLookFor === "todos"}
+  >
+    <option value="" hidden>
+      {toLookFor === "specialty"
+        ? "Selecciona una especialidad"
+        : "Selecciona una ubicación"}
+    </option>
+    {uniqueValues.map((value, index) => (
+      <option value={value} key={index}>
+        {value}
       </option>
-      {dentists.map((item, index) => (
-        <option value={item[toLookFor]} key={index}>
-          {item[toLookFor]}
-        </option>
-      ))}
-    </select>
-    <span className="consults-filter-change">
-        <Button buttonText="Ver mis citas"/>
-      </span>
-  </div>
+    ))}
+  </select>
+        </span>
+      </div>
+    </div>
   );
 };
 
