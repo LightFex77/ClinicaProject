23acab59078f34bc6dd0f93096ee28365587ea87
LightFex77 <max706genesoviedo@gmail.com>
2023-09-04 13:32:17 -0400

    Importamos con exito el estilo en el app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import Home from "./components/home/Main"
+import Home from "./components/home/Main";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/home/Header";
 import Footer from "./components/home/Footer";
 import Consults from "./components/consults/Consults";
 import "./styles/elements.css";
 import "./styles/consults.css";
+import "./styles/modalsOfConsults.css";
 
 const App = () => {
   return (
