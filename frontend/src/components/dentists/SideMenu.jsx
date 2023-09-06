b25d6bff18b9c9bde6c8d03143e6fa539fd4d559
LightFex77 <max706genesoviedo@gmail.com>
2023-09-05 20:17:57 -0400

    Agregando estilos al apartado de dentistas

--- frontend/src/components/dentists/SideMenu.jsx
+++ frontend/src/components/dentists/SideMenu.jsx
@@ -1,20 +1,49 @@
-import {NavLink} from "react-router-dom"
-import "../../styles/dentists/dentists.css"
+import { NavLink } from "react-router-dom";
+import "../../styles/dentists/dentists.css";
 const SideMenu = () => {
   const classChange = (classObject) => {
-    console.log(classObject)
-    return classObject ? "active-link": "inactive-link"
-  }
+    console.log(classObject);
+    return classObject ? "active-link" : "inactive-link";
+  };
   return (
     <div className="side-menu-container">
       <ul>
-        <li><NavLink className={({isActive}) => classChange(isActive)}to="/dentists-panel/profile">Perfil</NavLink></li>
-        <li><NavLink className={({isActive}) => classChange(isActive)} to="/dentists-panel/schedules">Horarios</NavLink></li>
-        <li><NavLink className={({isActive}) => classChange(isActive)} to="/dentists-panel/quotes">Citas</NavLink></li>
-        <li>Cerrar Sesion</li>
+        <li>
+          <NavLink
+            className={({ isActive }) => classChange(isActive)}
+            to="/dentists-panel/profile"
+          >
+            Perfil
+            <i className="fa-regular fa-user"></i>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => classChange(isActive)}
+            to="/dentists-panel/schedules"
+          >
+            Horarios
+            <i className="fa-solid fa-calendar-days"></i>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => classChange(isActive)}
+            to="/dentists-panel/quotes"
+          >
+            Agenda
+            <i className="fa-solid fa-book-medical"></i>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={({ isActive }) => classChange(isActive)} to="/">
+            Cerrar Sesion
+            <i className="fa-solid fa-arrow-right-from-bracket"></i>
+          </NavLink>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SideMenu
+export default SideMenu;
